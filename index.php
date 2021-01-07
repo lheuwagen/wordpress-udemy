16dc46395cbe96326ef524716f04829c0273b97e
Liam <liam.heuwagen@eurosoft.net>
2021-01-07 10:09:10 +0100

    update

---
+++ index.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="de">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=deice-width, initial-scale=1.0">
+
+	<title>Titel der Seite</title>
+
+	<link rel="stylesheet" href="<?php bloginfo('template_url');?>/css/normalize.css">
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url');?>">
+</head>
+
+<body>
+	<header class="site-header">
+		<a href="#">
+			Logo - Beschreibung
+		</a>
+		<nav>
+			Navigation
+		</nav>
+	</header>
+
+	<main class="site-main">
+		<article class="site-content">
+			<h1>Hauptinhalt</h1>
+			<p>Text</p>
+		</article>
+		<aside class="site-sidebar">
+			Seitenleiste
+		</aside>
+	</main>
+
+	<footer class="site-footer">
+		<nav>
+			Navigation
+		</nav>
+		<nav>
+			Navigation
+		</nav>
+	</footer>
+</body>
+
+</html>
